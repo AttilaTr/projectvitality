42fcf12bee0d48adb0d5e2479b9a08a6581671c4
AttilaTr <attila.trekovanicz@gmail.com>
2021-06-03 13:02:17 +0000

    App V.1 is completed

--- server/app.py
+++ server/app.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 db = SQLAlchemy(app)
 
+
 class Days(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     day = db.Column(db.String(50), nullable=False)
     indoor = db.Column(db.String(50), nullable=False)
     improvement = db.Column(db.Integer, nullable=False)
+    message = db.Column(db.String(100), nullable=False)
+
 
 @app.route('/')
 def home():
@@ -21,19 +24,27 @@ def home():
     indoor = requests.get('http://msindoor_api:5000/get_indoor')
     improvementindoor = requests.post('http://msindoor_api:5000/get_improvement', data=indoor.text)
     improvement = int(improvementday.text) + int(improvementindoor.text)
-    
+
+    if improvement >= 5:
+        message = 'This activity is much recommended to you.'
+    elif improvement <= 0:
+        message = 'Please avoid this activity on the mentioned day.'
+    else:
+        message = 'This activity would neither increase, nor decrease your vitality level.'
+
     days_imp = Days.query.order_by(desc(Days.id)).limit(5).all()
-    
+
     db.session.add(
         Days(
-            day = day.text,
-            indoor = indoor.text,
-            improvement = improvement
-        )
+            day=day.text,
+            indoor=indoor.text,
+            improvement=improvement,
+            message=message
+            )
     )
     db.session.commit()
 
-    return render_template('index.html', day=day.text, indoor=indoor.text, improvement=improvement, days_imp=days_imp)
+    return render_template('index.html', day=day.text, indoor=indoor.text, improvement=improvement, message=message, days_imp=days_imp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
