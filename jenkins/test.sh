c70cf8ed32483b5de1a3c9acb09f93ec87d305f6
AttilaTr <attila.trekovanicz@gmail.com>
2021-06-04 14:10:35 +0000

    Jenkinsfile4

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,6 +6,6 @@ source venv/bin/activate
 
 pip3 install -r server/requirements.txt
 
-python3 -m pytest server --cov-report=xml --junitxml=junit/test-results.xml
-python3 -m pytest msdays --cov-report=xml --junitxml=junit/test-results.xml
-python3 -m pytest msindoor --cov-report=xml --junitxml=junit/test-results.xml
+python3 -m pytest server --cov=application --cov-report=xml  --junitxml=junit/test-results.xml
+python3 -m pytest msdays --cov=application --cov-report=xml  --junitxml=junit/test-results.xml
+python3 -m pytest msindoor --cov=application --cov-report=xml  --junitxml=junit/test-results.xml
