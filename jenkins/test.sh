589bbf84825b1a981f45d9e9f6ef997acea945a2
AttilaTr <attila.trekovanicz@gmail.com>
2021-06-04 13:19:38 +0000

    Jenkinsfile3

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -4,6 +4,6 @@ sudo apt-get install python3 python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 
-pip3 install -r requirements.txt
+pip3 install -r server/requirements.txt
 
-python3 -m pytest --cov=application --cov-report=xml --junitxml=junit/test-results.xml
+python3 -m pytest server --cov=application --cov-report=xml --junitxml=junit/test-results.xml
