8a6d7471965c20bbf23b56a2db07acd3c79b4780
AttilaTr <attila.trekovanicz@gmail.com>
2021-06-04 15:15:28 +0000

    Jenkinsfile5

--- jenkins/dockerbuild.sh
+++ jenkins/dockerbuild.sh
@@ -5,3 +5,9 @@ curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins
 
 docker login -u $DOCKER_LOGIN_USR -p $DOCKER_LOGIN_PSW
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose up -d --build alexeipc/server-app:latest
